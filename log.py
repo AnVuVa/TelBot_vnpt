7a74316f60d004c421674a6002b9e15e7418ebc0
AnVuVa <vuan141592@gmail.com>
2025-11-06 11:43:56 +0000

    logging file contain log level

--- log.py
+++ log.py
@@ -5,6 +5,6 @@ def log(message: str, log_level: str = "info", log_dir: str = LOG_DIR):
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
-    log_file = os.path.join(log_dir, "bot.log")
+    log_file = os.path.join(log_dir, f"bot_{log_level}.log")
     with open(log_file, "a", encoding="utf-8") as f:
         f.write(f"[{log_level.upper()}] {message}\n")
